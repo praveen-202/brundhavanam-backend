835ae431f6342f4b5ac722bfd3e958462951fd90
Praveen Kumar <kumarpraveen7604@gmail.com>
2025-12-29 11:24:22 +0530

    first commit

---
+++ src/main/java/com/brundhavanam/BrundhavanamApplication.java
@@ -0,0 +1,13 @@
+package com.brundhavanam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrundhavanamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrundhavanamApplication.class, args);
+	}
+
+}
