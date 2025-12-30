e583ee1b131464712aa307f5909a5b2c42753fe5
Praveen Kumar <kumarpraveen7604@gmail.com>
2025-12-30 12:10:28 +0530

    prv - Resolve all errors

--- src/main/java/com/brundhavanam/user/controller/UserController.java
+++ src/main/java/com/brundhavanam/user/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//new
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
